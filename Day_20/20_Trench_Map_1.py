4b4acdbe51c6774cbdd0ed7341d4d7fec3ebb90a
Chris Bowles <BowlesCR@users.noreply.github.com>
2021-12-20 11:15:01 -0500

    Day 20

---
+++ Day_20/20_Trench_Map_1.py
@@ -0,0 +1,83 @@
+import fileinput
+from functools import lru_cache
+
+alg: list[str]
+inp: dict[tuple[int, int], str]
+
+
+@lru_cache(maxsize=None)
+def get(pos: tuple[int, int], layer: int = 0) -> str:
+    r, c = pos
+    if layer == 0:
+        num = int(
+            "".join(
+                (
+                    inp.get((r - 1, c - 1), "0"),
+                    inp.get((r - 1, c), "0"),
+                    inp.get((r - 1, c + 1), "0"),
+                    inp.get((r, c - 1), "0"),
+                    inp.get((r, c), "0"),
+                    inp.get((r, c + 1), "0"),
+                    inp.get((r + 1, c - 1), "0"),
+                    inp.get((r + 1, c), "0"),
+                    inp.get((r + 1, c + 1), "0"),
+                )
+            ),
+            2,
+        )
+    else:
+        num = int(
+            "".join(
+                (
+                    get((r - 1, c - 1), layer - 1),
+                    get((r - 1, c), layer - 1),
+                    get((r - 1, c + 1), layer - 1),
+                    get((r, c - 1), layer - 1),
+                    get((r, c), layer - 1),
+                    get((r, c + 1), layer - 1),
+                    get((r + 1, c - 1), layer - 1),
+                    get((r + 1, c), layer - 1),
+                    get((r + 1, c + 1), layer - 1),
+                )
+            ),
+            2,
+        )
+
+    num = int("".join(num), 2)
+
+    if alg[num] == "#":
+        return "1"
+    else:
+        return "0"
+
+
+def main() -> None:
+    global alg, inp
+    lines = [line.strip() for line in fileinput.input()]
+
+    alg = lines[0]
+
+    dim = len(lines) - 2
+    inp = {}
+    for r in range(2, len(lines)):
+        for c in range(len(lines[r])):
+            if lines[r][c] == "#":
+                inp[(r - 2, c)] = "1"
+            # Don't bother storing the 0's, we're just going to imply it later
+
+    del lines
+
+    DEPTH = 2
+    out: set[tuple[int, int]] = set()
+    for r in range(-DEPTH, dim + DEPTH):
+        for c in range(-DEPTH, dim + DEPTH):
+
+            p = get((r, c), DEPTH - 1)
+            if p == "1":
+                out.add((r, c))
+
+    print(len(out))
+
+
+if __name__ == "__main__":
+    main()
